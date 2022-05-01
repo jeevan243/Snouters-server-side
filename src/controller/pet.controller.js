3810926baf60c8d7419c3caf51a5f3aa64ab162d
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-05-01 20:57:13 +0530

    sorting added from backend

--- src/controller/pet.controller.js
+++ src/controller/pet.controller.js
@@ -7,7 +7,6 @@ router.post("/create", async (req, res) => {
     try {
         let pet = await Pets.create(req.body)
         return res.status(201).send(pet)
-
     } catch (er) {
         return res.status(400).send(er.message)
     }
@@ -17,10 +16,7 @@ router.post("/create", async (req, res) => {
 router.get("/all", async (req, res) => {
     try {
         let pets = await Pets.find()
-        console.log(pets)
         return res.status(200).send(pets)
-
-
     } catch (er) {
         return res.status(500).send(er.message)
     }
@@ -36,7 +32,7 @@ router.get("/:id", async (req, res) => {
     }
 })
 
-//Ascending
+// price Ascending
 router.get("/all/sort1", async (req, res) => {
     try {
         let pets = await Pets.find().sort({ costperday: 1 })
@@ -46,11 +42,31 @@ router.get("/all/sort1", async (req, res) => {
     }
 })
 
-//Descending
+//price Descending
 router.get("/all/sort-1", async (req, res) => {
     try {
         let pets = await Pets.find().sort({ costperday: -1 })
-        console.log(pets)
+        return res.status(200).send(pets)
+    } catch (er) {
+        return res.status(500).send(er.message)
+    }
+})
+
+
+//verified Ascending
+router.get("/all/rating1", async (req, res) => {
+    try {
+        let pets = await Pets.find().sort({ rating: 1 })
+        return res.status(200).send(pets)
+    } catch (er) {
+        return res.status(500).send(er.message)
+    }
+})
+
+//verified Decending
+router.get("/all/rating-1", async (req, res) => {
+    try {
+        let pets = await Pets.find().sort({ rating: -1 })
         return res.status(200).send(pets)
 
 
@@ -58,9 +74,4 @@ router.get("/all/sort-1", async (req, res) => {
         return res.status(500).send(er.message)
     }
 })
-
-
-
-
-
 module.exports = router;
